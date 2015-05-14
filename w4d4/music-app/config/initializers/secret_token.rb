d860ad6d7827a246f152b4d91b1f5a95759f1180
Aleksey Chebotarev <aleksey.chebotarev@gmail.com>
2015-05-14 10:22:03 -0700

    Initialize Music App for w4d4 solo day

---
+++ w4d4/music-app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicApp::Application.config.secret_key_base = '4c71a90c46abcd9a161a060831da1fa42a6c2d2f8f3de596f2ab5de83f21286943b02102a106b2777beebf150b5e04156f5206acdf1f27526a1922bbd5b9e3a1'
