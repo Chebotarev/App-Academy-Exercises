d0a87a58d2c8f7ab7b4b4aa329b60126278dcaad
Aleksey Chebotarev <aleksey.chebotarev@gmail.com>
2015-05-14 10:44:12 -0700

    Implement User model

--- w4d4/music-app/app/models/user.rb
+++ w4d4/music-app/app/models/user.rb
@@ -1,2 +1,38 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+    save!
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    save!
+  end
 end
