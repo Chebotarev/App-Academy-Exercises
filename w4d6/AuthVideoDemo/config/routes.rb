a00d150105e85059558cdba51798b77a021393f5
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-05-17 22:10:49 -0700

    Create Static Pages

--- w4d6/AuthVideoDemo/config/routes.rb
+++ w4d6/AuthVideoDemo/config/routes.rb
@@ -1,4 +1,8 @@
 AuthDemo::Application.routes.draw do
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
   resource :session, only: [:create, :destroy, :new]
   resource :user, only: [:create, :new, :show] do
     resource :counter, only: [:update]
