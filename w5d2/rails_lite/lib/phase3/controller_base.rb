39b5571ba0db68e7c5607f91bd6cb34bdfe25099
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-05-19 14:02:04 -0700

    Write ControllerBase#render to render html.erb templates from a file (Phase 3)

--- w5d2/rails_lite/lib/phase3/controller_base.rb
+++ w5d2/rails_lite/lib/phase3/controller_base.rb
@@ -8,6 +8,11 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      controller_name = self.class.to_s.underscore
+      contents = File.read("views/#{controller_name}/#{template_name}.html.erb")
+      erb_template = ERB.new(contents)
+      eval_content = erb_template.result(binding)
+      render_content(eval_content, "text/html")
     end
   end
 end
