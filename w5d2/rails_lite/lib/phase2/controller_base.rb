0605307f4cafe2d7e893ec26e482f25f19503ac0
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-05-19 13:41:04 -0700

    Implement ControllerBase render and redirect_to methods (Phase 2)

--- w5d2/rails_lite/lib/phase2/controller_base.rb
+++ w5d2/rails_lite/lib/phase2/controller_base.rb
@@ -4,20 +4,34 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
+
+      @already_built_response = false
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      raise "Already built response" if already_built_response?
+      res["Location"] = url
+      res.status = 302
+
+      @already_built_response = true
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, content_type)
+      raise "Already built response" if already_built_response?
+      res.content_type = content_type
+      res.body = content
+      @already_built_response = true
     end
   end
 end
