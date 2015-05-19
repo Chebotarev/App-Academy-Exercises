c702da6eb39ae60587ec9754200ee0f78c1935d4
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-05-19 13:23:30 -0700

    Implement basic server

--- w5d2/rails_lite/bin/p01_basic_server.rb
+++ w5d2/rails_lite/bin/p01_basic_server.rb
@@ -4,3 +4,15 @@ require 'webrick'
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
+
+server = WEBrick::HTTPServer.new(Port: 3000)
+
+trap('INT') do
+  server.shutdown
+end
+
+server.mount_proc("/") do |req, res|
+  res.body = req.path
+end
+
+server.start
